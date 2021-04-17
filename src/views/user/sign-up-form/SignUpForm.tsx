3caf6233f4b696cf15a17311ab7f6224600b47d8
Jorge Moreira <jorge.smrr@gmail.com>
2021-04-17 14:52:49 -0300

    Remove room step from sign up form

--- src/views/user/sign-up-form/SignUpForm.tsx
+++ src/views/user/sign-up-form/SignUpForm.tsx
@@ -1,43 +1,22 @@
 import React from "react";
-import RoomCreatorConnect from "../../room/room-creator/RoomCreatorConnect";
-import RoomInviteConfirmationConnect from "../../room/room-invite-confirmation/RoomInviteConfirmationConnect";
 import UsernameFormConnect from "../username-form/UsernameFormConnect";
 import "./SignUpForm.css";
 
 export interface SignUpFormProps {
     isConnected: boolean;
     isBlocked: boolean;
-    roomIdToJoin?: string;
-    onDismissRoomId: () => void;
 }
 
-const SignUpForm: React.FC<SignUpFormProps> = ({
-    isConnected,
-    isBlocked,
-    roomIdToJoin,
-    onDismissRoomId,
-}) => {
+const SignUpForm: React.FC<SignUpFormProps> = ({ isConnected, isBlocked }) => {
     if (isBlocked) {
         return (
             <div>You've reached the maximum number of active connections.</div>
         );
     } else {
-        const renderedRoomStep = roomIdToJoin ? (
-            <RoomInviteConfirmationConnect
-                roomId={roomIdToJoin}
-                onDone={onDismissRoomId}
-            />
-        ) : (
-            <RoomCreatorConnect />
-        );
-
         return (
             <div className="signup__form">
                 <div className="signup__form__step">
-                    <div className="font-extra-bold">
-                        {isConnected ? 2 : 1})
-                    </div>
-                    {isConnected ? renderedRoomStep : <UsernameFormConnect />}
+                    <UsernameFormConnect />
                 </div>
             </div>
         );
--- src/views/user/sign-up-form/SignUpFormConnect.tsx
+++ src/views/user/sign-up-form/SignUpFormConnect.tsx
@@ -1,21 +1,12 @@
 import React from "react";
-import useUrlHash from "../../../hooks/useUrlHash";
 import { useAppSelector } from "../../../store/hooks";
 import SignUpForm from "./SignUpForm";
 
 const SignUpFormConnect: React.FC = () => {
     const isConnected = useAppSelector((state) => !!state.socket?.isConnected);
     const isBlocked = useAppSelector((state) => state.isSignInBlocked);
-    const { hash: roomIdToJoin, onClear: onDismissRoomId } = useUrlHash();
 
-    return (
-        <SignUpForm
-            isConnected={isConnected}
-            isBlocked={isBlocked}
-            roomIdToJoin={roomIdToJoin}
-            onDismissRoomId={onDismissRoomId}
-        />
-    );
+    return <SignUpForm isConnected={isConnected} isBlocked={isBlocked} />;
 };
 
 export default SignUpFormConnect;
